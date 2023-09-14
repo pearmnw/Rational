b5d768abeaedd16b25409a9a2f8542936d702e5a
WirapornSot <wiraporn.sot@student.mahidol.ac.th>
2023-09-15 01:30:21 +0700

    Commit ka

--- src/main/java/Rational.java
+++ src/main/java/Rational.java
@@ -1,43 +1,46 @@
-class Rational { 
-    long numerator,denominator; 
+class Rational {
+    long numerator,denominator;
 
-    class Illegal extends Exception { 
-        String reason; 
-        Illegal (String reason) { 
-            this.reason = reason; 
-        } 
-    } 
+    class Illegal extends Exception {
+        String reason;
+        Illegal (String reason) {
+            this.reason = reason;
+        }
+    }
 
     Rational() {
         // to be completed
+        super();
     }
 
-    Rational(long numerator, long denominator) throws Illegal { 
+    Rational(long numerator, long denominator) throws Illegal {
         // to be completed
-    } 
+        this.numerator = numerator;
+        this.denominator = denominator;
+    }
 
-    // find the reduce form 
-    private void simplestForm() { 
-        long computeGCD; 
-        computeGCD = GCD(Math.abs(numerator), denominator); 
-        numerator /= computeGCD; 
-        denominator /= computeGCD; 
-    } 
+    // find the reduce form
+    private void simplestForm() {
+        long computeGCD;
+        computeGCD = GCD(Math.abs(numerator), denominator);
+        numerator /= computeGCD;
+        denominator /= computeGCD;
+    }
 
-    // find the greatest common denominator 
-    private long GCD(long a, long b) { 
-        if (a%b ==0) return b; 
-        else return GCD(b,a%b); 
+    // find the greatest common denominator
+    private long GCD(long a, long b) {
+        if (a%b ==0) return b;
+        else return GCD(b,a%b);
     }
 
     /***
      * Compute an addition of the current rational number to another given rational number
      * @param x the rational number to be added to the current rational number
      */
-    public void add(Rational x) { 
+    public void add(Rational x) {
         numerator = (numerator * x.denominator) + (x.numerator * denominator);
-        denominator = (denominator * x.denominator); 
-        simplestForm(); 
+        denominator = (denominator * x.denominator);
+        simplestForm();
     }
 
     /***
@@ -46,14 +49,20 @@ class Rational {
      */
     public void subtract(Rational x) {
         // to be completed
+        numerator = (numerator * x.denominator) - (x.numerator * denominator);
+        denominator = (denominator * x.denominator);
+        simplestForm();
     }
 
     /***
      * Compute a multiplication of the current rational number to another given rational number
      * @param x the rational number to be multiplied to the current rational number
      */
-    public void multiply(Rational x) { 
+    public void multiply(Rational x) {
         // to be completed
+        numerator = (numerator * x.numerator);
+        denominator = (denominator * x.denominator);
+        simplestForm();
     }
 
     /***
@@ -62,6 +71,9 @@ class Rational {
      */
     public void divide(Rational x) {
         // to be completed
+        numerator = (numerator * x.denominator);
+        denominator = (denominator * x.numerator);
+        simplestForm();
     }
 
     /***
@@ -71,7 +83,11 @@ class Rational {
      */
     public boolean equals(Object x) {
         // to be completed
-        return true; // TODO: This needs to be modified.
+        // TODO: This needs to be modified.
+        if (x == null) return false;
+        if (x.getClass() != this.getClass()) return false;
+        Rational b = (Rational) x;
+        return compareTo(b) == 0;
     }
 
     /***
@@ -82,19 +98,41 @@ class Rational {
      */
     public long compareTo(Object x) {
         // to be completed
-        return -1; // TODO: this needs to be modified.
+        // TODO: this needs to be modified.
+        Rational b = (Rational) x;
+        Rational a = this;
+        long lhs = a.numerator * b.denominator;
+        long rhs = a.denominator * b.numerator;
+        if (lhs < rhs) return -1;
+        if (lhs > rhs) return +1;
+        return 0;
     }
 
     /***
      * Give the formatted string of the rational number
      * @return the string representation of the rational number. For example, "1/2", "3/4".
      */
-    public String toString() { 
+    public String toString() {
         // to be completed
-        return ""; // TODO: This needs to be modified.
+        // TODO: This needs to be modified.
+        if(denominator == 1)
+            return Long.toString(numerator);
+        return Long.toString(numerator)+"/"+Long.toString(denominator);
     }
 
     public static void main(String[] args) {
         System.out.println("This is Rational class.");
+
+        Rational x = new Rational();
+        x.numerator = 1;
+        x.denominator = 2;
+        Rational y = new Rational();
+        y.numerator = 1;
+        y.denominator = 4;
+//        x.add(y);
+//        x.subtract(y);
+        x.multiply(y);
+//        x.divide(y);
+        System.out.println(x.toString());
     }
 }
--- src/test/java/RationalTest.java
+++ src/test/java/RationalTest.java
@@ -12,5 +12,33 @@ public class RationalTest {
         y.denominator = 4;
         x.add(y);
         Assert.assertEquals(3, x.numerator);
+        Assert.assertEquals(4, x.denominator);
+    }
+
+    @Test
+    public void testSubtract() throws Rational.Illegal {
+        Rational x = new Rational(4,8);
+        Rational y = new Rational(1,6);
+        x.subtract(y);
+        Assert.assertEquals(1, x.numerator);
+        Assert.assertEquals(3, x.denominator);
+    }
+
+    @Test
+    public void testMultiply() throws Rational.Illegal{
+        Rational x = new Rational(4,17);
+        Rational y = new Rational(17,4);
+        x.multiply(y);
+        Assert.assertEquals(1, x.numerator);
+        Assert.assertEquals(1, x.denominator);
+    }
+
+    @Test
+    public void testDivide() throws Rational.Illegal{
+        Rational x = new Rational(3,5);
+        Rational y = new Rational(3,8);
+        x.divide(y);
+        Assert.assertEquals(8, x.numerator);
+        Assert.assertEquals(5, x.denominator);
     }
 }
